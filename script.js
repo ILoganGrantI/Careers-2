3d71a2a4b39910656bbd83aef1e1cb62d0aea306
Logan Grant <logan.grant1@ocsbstudent.ca>
2021-03-18 16:55:36 +0000

    --

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById('change-image').addEventListener('click', image)
+
+document.getElementById('change-image1').addEventListener('click', image1)
+
+document.getElementById('change-image2').addEventListener('click', image2)
+
+document.getElementById('change-image3').addEventListener('click', image3)
+
+document.getElementById('change-image4').addEventListener('click', image4)
+
+function image () {
+  document.getElementById('picture').src = 'Vocation.png'
+}
+
+function image1 () {
+  document.getElementById('picture').src = 'Strengths.png'
+}
+
+function image2 () {
+  document.getElementById('picture').src = 'passion.png'
+}
+
+function image3 () {
+  document.getElementById('picture').src = 'career.png'
+}
+
+function image4 () {
+  document.getElementById('picture').src = 'cummunity.png'
+}
